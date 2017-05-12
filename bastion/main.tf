4f08fb55c05bb967eb151c880a8e6ca113a3c3df
Yakhya Dabo <yakhyadabo@gmail.com>
2017-05-12 10:43:56 +0100

    Create folder bastion

--- bastion/main.tf
+++ bastion/main.tf
@@ -1,5 +1,6 @@
 module "bastion" {
-  source = "modules/bastion"
+  source = "../modules/bastion"
+
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
   allowed_network = "${var.allowed_network}"
