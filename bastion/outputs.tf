42966b277cb29bdeb9b840d88a6b208d350c34d2
Yakhya Dabo <yakhyadabo@gmail.com>
2017-05-20 12:02:21 +0100

    bastion: add outputs

---
+++ bastion/outputs.tf
@@ -0,0 +1,7 @@
+output "bastion_id" {
+  value = "${module.bastion.id}$"
+}
+
+output "bastion_name" {
+  value = "${module.bastion.name}$"
+}
---
+++ modules/bastion/outputs.tf
@@ -0,0 +1,7 @@
+output "id" {
+  value = "${aws_instance.bastion.id}$"
+}
+
+output "name" {
+  value = "${aws_instance.bastion.name}$"
+}
