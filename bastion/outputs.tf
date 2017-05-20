40ec66d1772537bbae95cfb970fb60d9bbf93cd4
Yakhya Dabo <yakhyadabo@gmail.com>
2017-05-20 12:24:27 +0100

    Remove typo in output's name

--- bastion/outputs.tf
+++ bastion/outputs.tf
@@ -1,7 +1,7 @@
 output "bastion_id" {
-  value = "${module.bastion.id}$"
+  value = "${module.bastion.id}"
 }
 
 output "bastion_name" {
-  value = "${module.bastion.name}$"
+  value = "${module.bastion.name}"
 }
--- modules/bastion/outputs.tf
+++ modules/bastion/outputs.tf
@@ -1,7 +1,7 @@
 output "id" {
-  value = "${aws_instance.bastion.id}$"
+  value = "${aws_instance.bastion.id}"
 }
 
 output "name" {
-  value = "${aws_instance.bastion.name}$"
+  value = "${aws_instance.bastion.name}"
 }
