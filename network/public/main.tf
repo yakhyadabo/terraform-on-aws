be6acd14d0027e890f3d0224128cc54dc0f2ef9f
Yakhya Dabo <yakhyadabo@gmail.com>
2017-05-22 13:54:00 +0100

    Fix typo in public module's name

--- network/public/main.tf
+++ network/public/main.tf
@@ -1,5 +1,5 @@
-module "dmz" {
-  source = "../../modules/network/dmz"
+module "public" {
+  source = "../../modules/network/public"
 
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
