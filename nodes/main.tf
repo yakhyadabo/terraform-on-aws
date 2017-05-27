ca5ca74d039ed81ed23b8d25c628b80ed57719ae
Yakhya Dabo <yakhyadabo@gmail.com>
2017-05-27 14:35:48 +0100

    Comment unused vars

--- nodes/main.tf
+++ nodes/main.tf
@@ -1,7 +1,7 @@
 module "node" {
   source = "../modules/nodes"
 
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  allowed_network = "${var.allowed_network}"
+#  access_key = "${var.access_key}"
+#  secret_key = "${var.secret_key}"
+#  allowed_network = "${var.allowed_network}"
 }
