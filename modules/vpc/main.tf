ff6cea8641e07df68813486f16c9fcaf6358a557
Yakhya Dabo <yakhyadabo@gmail.com>
2017-05-27 14:34:46 +0100

    Put vpc's cidr in a variable

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -2,7 +2,7 @@
 # VPC
 ##
 resource "aws_vpc" "test" {
-    cidr_block = "10.0.0.0/16"
+  cidr_block = "${var.vpc_cidr}"
 }
 
 resource "aws_internet_gateway" "gateway" {
---
+++ modules/vpc/var.tf
@@ -0,0 +1,3 @@
+variable "vpc_cidr" {
+    description = "CIDR of VPC"
+}
