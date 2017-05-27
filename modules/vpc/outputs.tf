1287a5f046c9037dc6eca368bcd0d22e4364a495
Yakhya Dabo <yakhyadabo@gmail.com>
2017-05-27 16:06:20 +0100

    Expose new outputs, subnet_ip, bestion_sg, ...

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -6,6 +6,18 @@ output "gateway" {
   value = "${aws_internet_gateway.gateway.id}"
 }
 
-output "bastion" {
+output "bastion_ip" {
     value = "${aws_instance.bastion.public_ip}"
+}
+
+output "bastion_sg"{
+  value = "${aws_security_group.allow_bastion.id}"
+}
+
+output "private_network_id" {
+  value = "${aws_subnet.private.id}"
+}
+
+output "public_network_id" {
+  value = "${aws_subnet.public.id}"
 }
--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,12 +1,23 @@
-output "bastion_ip" {
-    value = "${module.vpc.bastion}"
-}
-
-
 output "vpc_id" {
   value = "${module.vpc.id}"
 }
 
+output "bastion_ip" {
+    value = "${module.vpc.bastion_ip}"
+}
+
+output "bastion_sg" {
+    value = "${module.vpc.bastion_sg}"
+}
+
 output "gateway_id" {
   value = "${module.vpc.gateway}"
+}
+
+output "private_network_id" {
+  value = "${module.vpc.private_network_id}"
+}
+
+output "public_network_id" {
+  value = "${module.vpc.public_network_id}"
 }
