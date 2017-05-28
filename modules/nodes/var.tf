ff22bf4937042ae3f423edfc332bad62f665f335
Yakhya Dabo <yakhyadabo@gmail.com>
2017-05-28 13:49:15 +0100

    Remove unused variables in Node module

--- modules/nodes/var.tf
+++ modules/nodes/var.tf
@@ -27,24 +27,3 @@ variable "amazon_amis" {
         us-west-2 = "ami-b5a7ea85"
     }
 }
-
-# variable "amazon_nat_amis" {
-#     description = "Amazon Linux NAT AMIs"
-#     default = {
-#         us-west-2 = "ami-bb69128b"
-#     }
-# }
-# 
-# variable "centos7_amis" {
-#     description = "CentOS 7 AMIs"
-#     default = {
-#         us-east-1 = "ami-96a818fe"
-#         us-west-2 = "ami-c7d092f7"
-#         us-west-1 = "ami-6bcfc42e"
-#         eu-west-1 =  "ami-e4ff5c93"
-#         ap-southeast-1 = "ami-aea582fc"
-#         ap-southeast-2 = "ami-bd523087"
-#         ap-northeast-1 = "ami-89634988"
-#         sa-east-1 = "ami-bf9520a2"
-#     }
-# }
--- nodes/main.tf
+++ nodes/main.tf
@@ -1,7 +1,3 @@
 module "node" {
   source = "../modules/nodes"
-
-#  access_key = "${var.access_key}"
-#  secret_key = "${var.secret_key}"
-#  allowed_network = "${var.allowed_network}"
 }
