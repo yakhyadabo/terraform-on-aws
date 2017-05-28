7ea27da4654033afe80d1d9cf7b47ec1e7c9173e
Yakhya Dabo <yakhyadabo@gmail.com>
2017-05-28 13:36:42 +0100

    Remove unused variables

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,8 +1,8 @@
 module "vpc" {
   source = "../modules/vpc"
 
-  access_key = "${var.access_key}" 
-  secret_key = "${var.secret_key}"
+#  access_key = "${var.access_key}" 
+#  secret_key = "${var.secret_key}"
   allowed_network = "${var.allowed_network}"
 
 #  vpc_cidr = "${var.vpc_cidr}"
