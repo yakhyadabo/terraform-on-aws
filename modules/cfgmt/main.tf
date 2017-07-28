a61073cb4b459949bccee125db6939134f11d219
Yakhya Dabo <yakhyadabo@gmail.com>
2017-07-28 15:57:23 +0100

    Setup host in public network

--- modules/cfgmt/main.tf
+++ modules/cfgmt/main.tf
@@ -4,53 +4,76 @@
 data "terraform_remote_state" "vpc" {
     backend = "s3"
     config {
-        bucket = "terraform-remote-state-for-bastion"
+        bucket = "terraform-remote-state-zeta"
         key = "vpc/terraform.tfstate"
         region = "${var.region}"
     }
 }
 
-resource "aws_security_group" "cfgmt" {
-    name = "cfgmt"
+/* Security group for the web */
+resource "aws_security_group" "cfgmt_web" {
+  name = "cfgmt_web"
+  description = "Security group for web that allows web traffic from internet"
+  vpc_id = "${data.terraform_remote_state.vpc.vpc_id}"
+
+ ingress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    self        = true
+  }
+
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    self        = true
+  }
+
+  tags { 
+    Name = "web-airpair-example" 
+  }
+}
+
+# Nat SG : 
+resource "aws_security_group" "cfgmt_nat" {
+    name = "cfgmt_nat"
     description = "Config management ....."
     vpc_id = "${data.terraform_remote_state.vpc.vpc_id}"
 
-    ingress {
-        from_port = 53
-        to_port = 53
-        protocol = "tcp"
-        self = true
-    }
-    ingress {
-        from_port = 53
-        to_port = 53
-        protocol = "udp"
-        self = true
-    }
-    ingress {
-        from_port = 8300
-        to_port = 8302
-        protocol = "tcp"
-        self = true
-    }
-    ingress {
-        from_port = 8301
-        to_port = 8302
-        protocol = "udp"
-        self = true
-    }
-    ingress {
-        from_port = 8400
-        to_port = 8400
-        protocol = "tcp"
-        self = true
-    }
-    ingress {
-        from_port = 8500
-        to_port = 8500
-        protocol = "tcp"
-        self = true
-    }
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["10.0.201.0/24"]
+  }
+
+  ingress {
+    from_port = 1194
+    to_port   = 1194
+    protocol  = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  tags { 
+    Name = "nat-airpair-example" 
+  }
 }
 
 resource "aws_instance" "cfgmt" {
@@ -60,11 +83,18 @@ resource "aws_instance" "cfgmt" {
     }
     ami = "${lookup(var.amazon_amis, var.region)}"
     instance_type = "t2.micro"
+    user_data = "${file("cloud-config/app.yml")}"
+   # user_data = <<-EOF
+   #           #!/bin/bash -v
+   #           sudo yum install epel-release
+   #           sudo yum install ansible > /tmp/ansible.log
+   #           EOF
     count = 1
     key_name = "${var.key_name}"
     security_groups = [
-        "${data.terraform_remote_state.vpc.bastion_sg}",
-        "${aws_security_group.cfgmt.id}"
+       # "${data.terraform_remote_state.vpc.bastion_sg}",
+        "${aws_security_group.cfgmt_nat.id}",
+        "${aws_security_group.cfgmt_web.id}"
     ]
     subnet_id =  "${data.terraform_remote_state.vpc.public_network_id}"
     private_ip = "10.0.0.10"
@@ -74,4 +104,5 @@ resource "aws_instance" "cfgmt" {
         role = "dns"
         environment = "test"
     }
+
 }
