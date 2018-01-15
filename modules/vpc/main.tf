d2f18cf0a75cbe411911229de05eb0bddb892f82
Yakhya Dabo <yakhyadabo@gmail.com>
2018-01-15 16:22:37 +0000

    [vpc] Add NAT Gateway

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -81,17 +81,53 @@ resource "aws_route_table_association" "public" {
 resource "aws_subnet" "private" {
     vpc_id = "${aws_vpc.test.id}"
     cidr_block = "10.0.1.0/24"
+    map_public_ip_on_launch = false
 }
 
-resource "aws_route_table" "private" {
-    vpc_id = "${aws_vpc.test.id}"
-    route {
-        cidr_block = "0.0.0.0/0"
-        instance_id = "${aws_instance.bastion.id}"
-    }
+## resource "aws_route_table" "private" {
+##     vpc_id = "${aws_vpc.test.id}"
+##     route {
+##         cidr_block = "0.0.0.0/0"
+##         instance_id = "${aws_instance.bastion.id}"
+##     }
+## }
+
+resource "aws_eip" "nat_eip" {
+  # count    = "${length(split(",", var.public_ranges))}"
+  count    = "${length(split(",", "10.0.0.0/24"))}"
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  # count = "${length(split(",", var.public_ranges))}"
+  count = "${length(split(",", "10.0.0.0/24"))}"
+  allocation_id = "${element(aws_eip.nat_eip.*.id, count.index)}"
+  subnet_id = "${element(aws_subnet.public.*.id, count.index)}"
+  depends_on = ["aws_internet_gateway.gateway"]
 }
 
 resource "aws_route_table_association" "private" {
     subnet_id = "${aws_subnet.private.id}"
     route_table_id = "${aws_route_table.private.id}"
+}
+
+
+# for each of the private ranges, create a "private" route table.
+resource "aws_route_table" "private" {
+  vpc_id = "${aws_vpc.test.id}"
+  # count = "${length(compact(split(",", var.private_ranges)))}"
+  count = "${length(compact(split(",", "10.0.1.0/24")))}"
+ # tags { 
+ #   Name = "${var.env}_private_subnet_route_table_${count.index}"
+ # }
+}
+
+# add a nat gateway to each private subnet's route table
+resource "aws_route" "private_nat_gateway_route" {
+  # count = "${length(compact(split(",", var.private_ranges)))}"
+  count = "${length(compact(split(",", "10.0.1.0/24")))}"
+  route_table_id = "${element(aws_route_table.private.*.id, count.index)}"
+  destination_cidr_block = "0.0.0.0/0"
+  depends_on = ["aws_route_table.private"]
+  nat_gateway_id = "${element(aws_nat_gateway.nat_gw.*.id, count.index)}"
 }
