8c7da9b940089a728eebeb4b766f426d51ed0954
Yakhya Dabo <yakhyadabo@gmail.com>
2018-01-19 16:35:38 +0000

    [main] Add public and private network

--- main.tf
+++ main.tf
@@ -13,33 +13,38 @@ module "vpc" {
   sg_nat = "${module.sec-group.sg_nat}"
 }
 
-## module "cfgmt" {
-##   source = "modules/asg"
-## 
-##   vpc_id = "${module.vpc.id}"
-##   subnet_id = "${module.vpc.public_network_id}"
-##   sg_web = "${module.sec-group.sg_web}"
-## 
-##   # min_instances_size = 5
-##   # max_instances_size = 10
-##   # server_port = 8080
-##   public_elb_sec_group = "${module.sec-group.public_elb_sec_group}"
-##   bastion_host_ssh_sec_group = "${module.sec-group.bastion_host_ssh}"
-## }
+module "cfgmt" {
+  source = "modules/asg"
+
+  vpc_id = "${module.vpc.id}"
+  subnet_id = "${module.vpc.public_network_id}"
+  elb_subnets = ["${module.vpc.public_network_id}"]
+  sg_web = "${module.sec-group.sg_web}"
+
+  # min_instances_size = 5
+  # max_instances_size = 10
+  # server_port = 8080
+  # public_elb_sec_group = ["${module.sec-group.public_elb_sec_group}", "${module.sec-group.sg_default}"]
+  public_elb_sec_group = ["${module.sec-group.public_elb_sec_group}"]
+  bastion_host_ssh_sec_group = "${module.sec-group.bastion_host_ssh}"
+  elb_suffix = "cfgmt"
+}
 
 module "internal" {
   source = "modules/asg"
 
   vpc_id = "${module.vpc.id}"
   subnet_id = "${module.vpc.private_network_id}"
-  elb_subnet_id = "${module.vpc.public_network_id}"
+  elb_subnets = ["${module.vpc.public_network_id}"]
   sg_web = "${module.sec-group.sg_web}"
 
   # min_instances_size = 5
   # max_instances_size = 10
   # server_port = 8080
-  public_elb_sec_group = "${module.sec-group.public_elb_sec_group}"
+  # public_elb_sec_group = ["${module.sec-group.public_elb_sec_group}", "${module.sec-group.sg_default}"]
+  public_elb_sec_group = ["${module.sec-group.public_elb_sec_group}"]
   bastion_host_ssh_sec_group = "${module.sec-group.bastion_host_ssh}"
+  elb_suffix = "internal"
 }
 
 module "sec-group" {
