b5c9878b56e008409170310db700ff81e4d916e8
Yakhya Dabo <yakhyadabo@gmail.com>
2018-01-19 16:31:40 +0000

    Set availability zones for ELB and the hosts
     - The subnet of the ELB and the subnet of the hosts should be in the
    same availability zone.
    
    See : https://aws.amazon.com/premiumsupport/knowledge-center/public-load-balancer-private-ec2/

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -58,6 +58,7 @@ resource "aws_subnet" "public" {
     vpc_id = "${aws_vpc.test.id}"
     cidr_block = "10.0.0.0/24"
     map_public_ip_on_launch = true
+    availability_zone = "us-west-2a"
 }
 
 resource "aws_route_table" "public" {
@@ -82,6 +83,7 @@ resource "aws_subnet" "private" {
     vpc_id = "${aws_vpc.test.id}"
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = false
+    availability_zone = "us-west-2a"
 }
 
 ## resource "aws_route_table" "private" {
