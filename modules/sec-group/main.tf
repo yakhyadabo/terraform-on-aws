423fc3d613aa62f108d08f92a5742a378a715c9e
Yakhya Dabo <yakhyadabo@gmail.com>
2018-01-19 16:30:05 +0000

    [sec-group] Add 8080 in bastion host for debugging

--- modules/sec-group/main.tf
+++ modules/sec-group/main.tf
@@ -78,6 +78,13 @@ resource "aws_security_group" "nat" {
   }
 
   egress {
+    from_port = 8080
+    to_port   = 8080
+    protocol  = "tcp"
+    cidr_blocks = ["${var.allowed_network}"]
+  }
+
+  egress {
     from_port = 443
     to_port   = 443
     protocol  = "tcp"
@@ -115,6 +122,13 @@ resource "aws_security_group" "bastion_host_ssh" {
     to_port   = 80
     protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress { ## Test connections //TODO Remove
+    from_port = 8080
+    to_port   = 8080
+    protocol  = "tcp"
+    cidr_blocks = ["${var.allowed_network}"]
   }
 
   egress { ## Enable curl httpS
