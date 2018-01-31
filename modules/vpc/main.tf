0d9c116d008342ab38dadbe8b9e03c38c6fdd9ae
Yakhya Dabo <yakhyadabo@gmail.com>
2018-01-31 15:16:19 +0000

    [vpc] Enable DNS hostname

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -2,7 +2,10 @@
 # VPC
 ##
 resource "aws_vpc" "test" {
-    cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  cidr_block = "10.0.0.0/16"
 }
 
 resource "aws_vpc_endpoint" "private-s3" {
