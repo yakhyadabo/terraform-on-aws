55af78f730a7a4a8f220aca248b6c5a682cc231f
Yakhya Dabo <yakhyadabo@gmail.com>
2018-02-06 16:46:58 +0000

    [postgres_rds] use route53 module

--- modules/postgres_rds/main.tf
+++ modules/postgres_rds/main.tf
@@ -1,66 +1,35 @@
-data "terraform_remote_state" "vpc" {
-  backend = "s3"
-  config {
-    bucket = "terraform-remote-state-zeta-dev"
-    key = "dev/vpc.tfstate"    
-    region = "us-west-2"
-  }
-}
-
-resource "aws_db_instance" "wordpress" {
-  name                    = "wordpress"
-  identifier              = "wordpress"
-  username                = "wordpress"
-  password                = "badpassword"
+resource "aws_db_instance" "postgres" {
+  name                    = "postgres"
+  identifier              = "postgres"
+  username                = "postgres"
+  password                = "password"
   port                    = 5432
   allocated_storage       = 256                                         # gigabytes
-  db_subnet_group_name    = "${aws_db_subnet_group.wordpress-pgsql.id}"
+  db_subnet_group_name    = "${aws_db_subnet_group.postgres-pgsql.id}"
   backup_retention_period = 7                                           # in days
   engine                  = "postgres"
   engine_version          = "9.5.4"
   instance_class          = "db.r3.large"
   multi_az                = false
 
-  #  parameter_group_name     = "mydbparamgroup1" # if you have tuned it
   publicly_accessible    = false
   skip_final_snapshot    = true
   storage_encrypted      = true                               # you should always do this
   storage_type           = "gp2"
-  vpc_security_group_ids = ["${aws_security_group.mydb1.id}"]
+  vpc_security_group_ids = ["${aws_security_group.postgres.id}"]
 }
 
-resource "aws_route53_zone" "internal" {
-  name   = "internal.com"
-  vpc_id = "${data.terraform_remote_state.vpc.vpc_id}"
-}
-
-resource "aws_route53_record" "internal-ns" {
-  zone_id = "${aws_route53_zone.internal.zone_id}"
-  name    = "internal.com"
-  type    = "NS"
-  ttl     = 30
-
-  records = [
-    "${aws_route53_zone.internal.name_servers.0}",
-    "${aws_route53_zone.internal.name_servers.1}",
-    "${aws_route53_zone.internal.name_servers.2}",
-    "${aws_route53_zone.internal.name_servers.3}",
-  ]
-
-  depends_on = ["aws_route53_zone.internal"]
-}
-
-resource "aws_route53_record" "wordpress-db" {
-  zone_id = "${aws_route53_zone.internal.zone_id}"
-  name    = "db.internal.com"
+resource "aws_route53_record" "postgres-db" {
+  zone_id = "${var.zone_id}"
+  name    = "app.db.zeta.com"
   type    = "CNAME"
   ttl     = "300"
-  records = ["${aws_db_instance.wordpress.address}"]
+  records = ["${aws_db_instance.postgres.address}"]
 }
 
-resource "aws_security_group" "mydb1" {
-  name = "mydb1"
-  vpc_id = "${data.terraform_remote_state.vpc.vpc_id}"
+resource "aws_security_group" "postgres" {
+  name = "postgres"
+  vpc_id = "${var.vpc_id}"
   description = "RDS postgres servers (terraform-managed)"
 
   # Only postgres in
@@ -71,7 +40,7 @@ resource "aws_security_group" "mydb1" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
+  ingress { ## TODO : Remove
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -87,7 +56,7 @@ resource "aws_security_group" "mydb1" {
   }
 }
 
-resource "aws_db_subnet_group" "wordpress-pgsql" {
-  name       = "wordpress-pgsql"
-  subnet_ids = ["${data.terraform_remote_state.vpc.private_subnet_ids}"]
+resource "aws_db_subnet_group" "postgres-pgsql" {
+  name       = "postgres-pgsql"
+  subnet_ids = ["${var.subnet_ids}"]
 }
--- modules/postgres_rds/variables.tf
+++ modules/postgres_rds/variables.tf
@@ -3,34 +3,11 @@ variable "vpc_id" {
   description = "Our default RDS virtual private cloud (rds_vpc)."
 }
 
-variable "private-db1_cidr" {
-  default     = "10.0.11.0/24"
-  description = "Your AZ"
+variable "subnet_ids" {
+  description = "Ids of the sunbets"
+  type = "list"
 }
 
-variable "private-db2_cidr" {
-  default     = "10.0.12.0/24"
-  description = "Your AZ"
+variable "zone_id" {
+  description = "Zone ID of Route53"
 }
-
-variable "az_1" {
-  default     = "us-west-2a"
-  description = "Your Az1, use AWS CLI to find your account specific"
-}
-
-variable "az_2" {
-  default     = "us-west-2b"
-  description = "Your Az2, use AWS CLI to find your account specific"
-}
-
-# variable "subnets_ids" {  
-#   type = "list"
-#   description = "The public subnets of our RDS VPC rds-vpc."
-# }
-
-
-# variable "rds_public_private-dbgroup" {  
-#   default = "default-vpc-XXXXXXXX"
-#   description = "Apparently the group name, according to the RDS launch wizard."
-# }
-
