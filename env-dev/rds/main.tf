eb584a9915f8af597e065e2934b2be28451cc639
Yakhya Dabo <yakhyadabo@gmail.com>
2018-02-06 16:43:58 +0000

    [storage] New forlder for RDS

--- env-dev/rds/main.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = "${var.region}"
-}
-
-module "wordpress_db" {
-  source = "../../modules/postgres_rds"
-}
-
--- env-dev/storage/app-db/backend.tf
+++ env-dev/storage/app-db/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "terraform-remote-state-zeta-dev"
-    key     = "dev/rds.tfstate"
+    key     = "dev/app.tfstate"
     region  = "us-west-2"
     encrypt = "true"
   }
