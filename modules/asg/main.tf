70e208517195ad36d2f5413cf585826f764549bb
Yakhya Dabo <yakhyadabo@gmail.com>
2018-02-20 08:20:04 +0000

    [asg] Get AMI from data source

--- modules/asg/main.tf
+++ modules/asg/main.tf
@@ -8,6 +8,27 @@ data "template_file" "docker_install" {
   # }
 }
 
+data "aws_ami" "centos7" {
+  owners      = ["679593333241"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["CentOS Linux 7 x86_64 HVM EBS *"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+
 resource "aws_launch_configuration" "cfgmt" {
   connection {
     user        = "centos"
@@ -15,7 +36,8 @@ resource "aws_launch_configuration" "cfgmt" {
   }
 
   key_name      = "${var.key_name}"
-  image_id      = "${lookup(var.centos7_amis, var.region)}"
+  # image_id      = "${lookup(var.centos7_amis, var.region)}"
+  image_id      = "${data.aws_ami.centos7.id}"
   instance_type = "t2.micro"
   user_data     = "${data.template_file.docker_install.rendered}"
 
