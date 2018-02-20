3fe3361b9190b437b296241f2c3f930f274cab07
Yakhya Dabo <yakhyadabo@gmail.com>
2018-02-20 18:46:32 +0000

    [asg] Remove unused variables

--- modules/asg/variables.tf
+++ modules/asg/variables.tf
@@ -42,21 +42,6 @@ variable "key_name" {
 # }
 # 
 
-variable "centos7_amis" {
-  description = "CentOS 7 AMIs"
-
-  default = {
-    us-east-1      = "ami-96a818fe"
-    us-west-1      = "ami-6bcfc42e"
-    us-west-2      = "ami-0c2aba6c"
-    eu-west-1      = "ami-e4ff5c93"
-    ap-southeast-1 = "ami-aea582fc"
-    ap-southeast-2 = "ami-bd523087"
-    ap-northeast-1 = "ami-89634988"
-    sa-east-1      = "ami-bf9520a2"
-  }
-}
-
 variable "allowed_network" {
   description = "The CIDR of network that is allowed to access the bastion host"
 }
