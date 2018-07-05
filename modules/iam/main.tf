520f394ba4becc5cf4030cd488e0e4da93eb3420
Yakhya Dabo <yakhyadabo@gmail.com>
2018-07-05 19:25:19 +0100

    [env-shared][iam] Add variables

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -3,6 +3,23 @@ resource "aws_iam_user" "zeta-admin" {
     path = "/"
 }
 
+resource "aws_iam_user_login_profile" "admin-login" {
+  count = "${var.create_user && var.create_iam_user_login_profile ? 1 : 0}"
+
+  user                    = "${aws_iam_user.zeta-admin.name}"
+  pgp_key                 = "${var.pgp_key}"
+  password_length         = "${var.password_length}"
+  password_reset_required = "${var.password_reset_required}"
+}
+
+resource "aws_iam_access_key" "admin-key" {
+  count = "${var.create_user && var.create_iam_access_key ? 1 : 0}"
+
+  user    = "${aws_iam_user.zeta-admin.name}"
+  pgp_key = "${var.pgp_key}"
+
+}
+
 resource "aws_iam_group" "admin-group" {
     name = "admin-group"
     path = "/"
