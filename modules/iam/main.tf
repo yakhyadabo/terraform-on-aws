1d3bcfc97822e6045f67355915a55583df2d8922
Yakhya Dabo <yakhyadabo@gmail.com>
2018-07-07 13:30:55 +0100

    [module][iam] Rename resources

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -3,7 +3,7 @@ resource "aws_iam_user" "zeta-admin" {
   path = "/"
 }
 
-resource "aws_iam_user_login_profile" "admin-login" {
+resource "aws_iam_user_login_profile" "zeta-admin" {
   count = "${var.create_user && var.create_iam_user_login_profile ? 1 : 0}"
 
   user                    = "${aws_iam_user.zeta-admin.name}"
@@ -12,31 +12,31 @@ resource "aws_iam_user_login_profile" "admin-login" {
   password_reset_required = "${var.password_reset_required}"
 }
 
-resource "aws_iam_access_key" "admin-key" {
+resource "aws_iam_access_key" "zeta-admin" {
   count = "${var.create_user && var.create_iam_access_key ? 1 : 0}"
 
   user    = "${aws_iam_user.zeta-admin.name}"
   pgp_key = "${var.pgp_key}"
 }
 
-resource "aws_iam_group" "admin-group" {
-  name = "admin-group"
+resource "aws_iam_group" "zeta-admin" {
+  name = "zeta-admin"
   path = "/"
 }
 
 resource "aws_iam_group_membership" "admin-user-membership" {
-  name = "admin-user-membership"
+  name = "zeta-admin"
 
   users = [
     "${aws_iam_user.zeta-admin.name}",
   ]
 
-  group = "${aws_iam_group.admin-group.name}"
+  group = "${aws_iam_group.zeta-admin.name}"
 }
 
 resource "aws_iam_group_policy" "explicit-admin" {
   name  = "explicit-admin"
-  group = "${aws_iam_group.admin-group.id}"
+  group = "${aws_iam_group.zeta-admin.id}"
 
   policy = <<EOF
 {
