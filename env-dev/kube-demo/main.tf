559432a7e0db9d939c6b77d8406a88c48397b089
Yakhya Dabo <yakhyadabo@gmail.com>
2018-07-14 23:12:20 +0100

    [env-dev][kube-demo] Create kubenetes cluster

---
+++ env-dev/kube-demo/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "${var.region}"
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config {
+    bucket = "terraform-remote-state-zeta-dev"
+    key    = "dev/vpc.tfstate"
+    region = "us-west-2"
+  }
+}
+
+module "kube-demo" {
+  source          = "../../modules/eks"
+
+  vpc_id             = "${data.terraform_remote_state.vpc.vpc_id}"
+  subnet_ids  = ["${data.terraform_remote_state.vpc.public_subnet_ids}"]
+}
