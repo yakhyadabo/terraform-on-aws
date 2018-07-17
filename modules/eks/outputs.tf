c5b3b92cd6cdc47a064c81c42fbdc9673d63c67c
Yakhya Dabo <yakhyadabo@gmail.com>
2018-07-17 17:20:44 +0100

    [module][eks] Use Heptio in kubeconfig

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -42,7 +42,7 @@ users:
   user:
     exec:
       apiVersion: client.authentication.k8s.io/v1alpha1
-      command: aws-iam-authenticator
+      command: heptio-authenticator-aws
       args:
         - "token"
         - "-i"
